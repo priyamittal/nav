548bd2ea62924e459926aab9255b6f4f59d52d5f
prmitt <priyamittal164@gmail.com>
2022-08-29 23:39:17 +0200

    initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+const dummyAds = [
+{
+        jobtitle: 1,
+        description: 1,
+        applicationUrl: "todo 1",
+        applicationDue: false,
+},
+{
+        jobtitle: 1,
+        description: 2,
+        applicationUrl: "todo 2",
+        applicationDue: false,
+},
+{
+        jobtitle: 1,
+        description: 3,
+        applicationUrl: "todo 3",
+        applicationDue: false,
+},
+];
+
+test("ads list", async () => {
+axios.get.mockResolvedValue({ data: dummyAds });
+render(<App />);
+
+const adList = await waitFor(() => screen.findAllByTestId("ad"));
+
+expect(adList).toHaveLength(3);
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [ads, setAds] = useState<any>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const count = 100
+
+    const fetchData = () => {
+        setIsLoading(true)
+        setError("")
+        fetch(`/public-feed/api/v1/ads?size=${count}`,
+            {
+              method: "GET",
+              headers: {
+                  "Authorization": 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ',
+                  "Content-Type": "application/json",
+              }
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error("Sorry something went wrong")
+                }
+            })
+            .then((data) => {
+                setIsLoading(false)
+                setAds(data);
+                console.log(data);
+             })
+             .catch((err) => {
+                setIsLoading(false)
+                setError(err.message)
+            });
+    };
+
+    console.log("ads:  " + ads)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button data-testid="fetchData" onClick={fetchData}>Fetch Ads</button>
+        {isLoading && <p data-testid="loading" >Loading...</p>}
+        {error && <p>{error}</p>}
+
+        {ads?.content?.length > 0 && (
+            <table>
+                <tr data-testid="ad">
+                    <th>Jobtitle</th>
+                    <th>Description</th>
+                    <th>ApplicationUrl</th>
+                    <th>ApplicationDue</th>
+                </tr>
+                {ads.content.map((val: any, key: any) => {
+                    return (
+                        <tr>
+                        <td >{val.jobtitle}</td>
+                        <td id="description" className="jobTitle">
+                            <div dangerouslySetInnerHTML={{ __html: val.description}}>
+                            </div>
+                        </td>
+                        <td className="breakword">{val.applicationUrl}</td>
+                        <td>{val.applicationDue}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        )}
     </div>
   );
 }
